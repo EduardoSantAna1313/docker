6b170476402b40d894ac262300125f3e0d917a54
Eduardo <eduardosantana1313@gmail.com>
2022-08-26 19:03:36 -0300

    refactoring run.bat

--- curso/08 - AWS CLI/run.bat
+++ curso/08 - AWS CLI/run.bat
@@ -15,6 +15,9 @@ docker rm -f %CONTAINER_NAME%
 
 docker rmi -f %CONTAINER_NAME%:v1
 
-docker build -t %CONTAINER_NAME%:v1 --build-arg AWS_ACCESS_KEY_ID=%aws_access_key_id% --build-arg AWS_SECRET_ACCESS_KEY=%aws_secret_access_key% --build-arg S3_FILE=%S3_FILE% .
+docker build -t %CONTAINER_NAME%:v1 ^
+    --build-arg AWS_ACCESS_KEY_ID=%aws_access_key_id% ^
+    --build-arg AWS_SECRET_ACCESS_KEY=%aws_secret_access_key% ^
+    --build-arg S3_FILE=%S3_FILE% .
 
 docker run --name %CONTAINER_NAME% %CONTAINER_NAME%:v1
